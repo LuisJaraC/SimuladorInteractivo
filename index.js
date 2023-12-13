fd18529c7a08b7eb1a544c18b069346680f9a26a
LuisJara <ljaraa1@gmail.com>
2023-12-13 01:59:44 -0300

    agregando arrow

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-
-let usuario = prompt("Ingrese su nombre")
-
-while(usuario){
-    let cuotas = parseInt(prompt("Bienvenido "+usuario+", ingrese el numero de cuotas para su vehiculo"))
-    let dinero = parseInt(prompt("Ingrese el monto de su vehiculo"))
-    
-    if(cuotas>0 && dinero>0){
-        let resultado = dinero / cuotas
-        alert(resultado)
-    }else {
-        alert("quizas te equivocaste, vuelve a intentarlo")
-    }
-    let confirmacion = prompt("¿Desea hacer otro calculo? s/n").toLowerCase()
-    if(confirmacion="n"){
-        alert("Muchas gracias, que tengas un excelente dia")
-        break
-    }
-=======
 
 let usuario = prompt("Ingrese su nombre")
 
@@ -38,5 +18,4 @@ while(usuario){
         alert("Muchas gracias, que tengas un excelente dia")
         break
     }
->>>>>>> b66fa91 (Agregando lo solicitado)
 }
