dfc8a19372faf166b03d83626327115f329c6f36
LuisJara <ljaraa1@gmail.com>
2023-12-13 01:28:33 -0300

    Agregando lo solicitado

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 let usuario = prompt("Ingrese su nombre")
 
@@ -16,4 +17,26 @@ while(usuario){
         alert("Muchas gracias, que tengas un excelente dia")
         break
     }
+=======
+
+let usuario = prompt("Ingrese su nombre")
+
+while(usuario){
+    let cuotas = parseInt(prompt("Hola "+usuario+", ingrese el numero de cuotas para su vehiculo"))
+    let dinero = parseInt(prompt("Ingrese el monto de su vehiculo"))
+    let division = (a,b) => a / b
+    if(cuotas>0 && dinero>0){
+        let cuotasVehiculo = division(dinero,cuotas)
+        alert(cuotasVehiculo)
+    }else {
+        alert("quizas te equivocaste, vuelve a intentarlo")
+    }
+    let confirmacion = prompt("¿Desea hacer otro calculo? s/n").toLowerCase()
+    if(confirmacion==="s"){
+        
+    } else {
+        alert("Muchas gracias, que tengas un excelente dia")
+        break
+    }
+>>>>>>> b66fa91 (Agregando lo solicitado)
 }
